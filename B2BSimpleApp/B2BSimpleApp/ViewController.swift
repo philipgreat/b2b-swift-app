518da6f714728c45dd1fa491f6f735ca2f0eb90c
philipgreat <philip_chang@163.com>
2016-08-05 23:47:19 +0800

    The inital version

---
+++ B2BSimpleApp/B2BSimpleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  B2BSimpleApp
+//
+//  Created by 张喜来 on 8/5/16.
+//  Copyright © 2016 张喜来. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
